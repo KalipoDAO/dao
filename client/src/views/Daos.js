79435401d18d46a2eb57b0d8bf8ce89e073976ff
Raphael Cornelis <raphael@moosty.com>
2021-05-14 21:40:12 +0200

    noflexcenter

--- client/src/views/Daos.js
+++ client/src/views/Daos.js
@@ -5,7 +5,7 @@ import {useDaos} from "../hooks/daos";
 export const Daos = () => {
   const {daos} = useDaos();
 
-  return (<div className="flex flex-row flex-wrap content-center justify-center space-x-5 space-y-8">
+  return (<div className="flex flex-row flex-wrap content-center  space-x-5 space-y-8">
     {daos?.map((dao, i) => <div className={[
       i === 0 && "ml-5 mt-8",
       "w-auto pr-5 ",
--- client/src/views/Members.js
+++ client/src/views/Members.js
@@ -5,7 +5,7 @@ import {MemberCard} from "@moosty/dao-storybook";
 export const Members = () => {
   const {members} = useMembers();
 
-  return (<div className="flex flex-row flex-wrap content-center justify-center space-x-5 space-y-8">
+  return (<div className="flex flex-row flex-wrap content-center space-x-5 space-y-8">
     {members?.map((member, i) => <div className={[
       i === 0 && "ml-5 mt-8",
       "w-auto pr-5 ",
