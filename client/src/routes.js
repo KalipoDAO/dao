607f96b73985a56b5c84d2288ec57171dab09f3c
Raphael Cornelis <raphael@moosty.com>
2021-05-15 10:33:55 +0200

    mt-filters

--- client/src/routes.js
+++ client/src/routes.js
@@ -35,7 +35,7 @@ export const Routes = () => {
         onRegisterClick={() => setCurrentOpen("register")}
       />
       <div className="w-full  min-h-screen  flex flex-col">
-        <div className={"w-full mx-auto md:w-app flex-grow my-6"}>
+        <div className={"w-full mx-auto md:w-app flex-grow mt-4"}>
         <PageTop updateFilters={updateFilters} filters={filtersFilter} />
         <Switch>
           <Route path={"/create-dao"}>
