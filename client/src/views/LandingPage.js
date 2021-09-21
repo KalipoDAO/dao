0835e71e8466f628326819b65192b9f15b60dc3f
jurrem <jurre@moosty.com>
2021-09-21 17:09:46 +0200

    homepageText

--- client/src/views/LandingPage.js
+++ client/src/views/LandingPage.js
@@ -33,21 +33,19 @@ export const LandingPage = () => {
       className={["flex flex-col lg:flex-row justify-between my-4 space-x-20  lg:my-10"].join(" ")}>
       <div className="flex flex-col w-full lg:w-1/2  mb-4">
         <Typography type="h1" Element="h1">Kalipo</Typography>
-        <Typography type="body" Element="span">Voting is a valuable governance tool. Votings give community members the
+        <Typography type="body" Element="span">A DAO platform to support online collaborations with the right tools, helpful insights and pre-made templates.
+          The current Proof of Concept provides voting as a valuable governance tool. This gives community members the
           possibility to exert influence and to express their viewpoints. However, votings consume time and energy of
           the voters and a voting committee. This often results in poor voter attendance and even in invalid votings.
-          Kalipo solves this problem by making votings easy. Kalipo powers the community!
+          Kalipo solves this problem by making votings easy.
         </Typography>
       </div>
       <div className="flex flex-col  w-full lg:w-1/2 ">
         <Typography type="h1" Element="h1">About the Kalipo team</Typography>
-        <Typography type="body" Element="span">The team consists of Xinrong Ding, Peter Nobels and <a
-          href="https://moosty.com/contact" target="_blank" rel="noopener noreferrer">Moosty</a> (Jurre, Raphael,
-          Sander). With different background, skillsets and experience we are on a journey to reinvent how organisations
-          work.
-          <a href="https://kalipo.com/contact" target="_blank" rel="noopener noreferrer" className="">
-            {` `}Reach out to us!
-          </a></Typography>
+        <Typography type="body" Element="span">Kalipo is a project run by a diverse group of professionals. With the Lisk Center Utrecht as their base. The technical side of the project is the responsibility of the Moosty team, a blockchain development studio. Xinrong Ding takes care that all our choices are user-centric. Peter Nobels combines the both with his broad knowledge of business concepts and ecosystems. Together, we make sure Kalipo is feasible, desirable, and viable.
+          <a href="https://lisk.chat/" target="_blank" rel="noopener noreferrer" className="hover:underline">
+            {` `}Reach out to us through Discord
+          </a>&nbsp; or follow us on <a href="https://twitter.com/KalipoDAO/" target="_blank" rel="noopener noreferrer" className="hover:underline">twitter</a> & <a href="https://kalipo.medium.com/" target="_blank" rel="noopener noreferrer" className="hover:underline">medium</a> </Typography>
       </div>
     </Container>
     <Container className={["flex", "flex-row "].join(" ")}>
